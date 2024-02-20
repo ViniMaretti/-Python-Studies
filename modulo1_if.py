4f0d43b58a7e5221ba64490bcf70fdfc803de5aa
Vinicius Mareti <110841400+ViniMaretti@users.noreply.github.com>
2024-02-20 10:52:59 -0300

    inicio do curso modulo if

---
+++ modulo1_if.py
@@ -0,0 +1,7 @@
+metaVendas = 50.000
+qtdVendas = float(input('Digite a quantidade de vendas do iphone'))
+
+if qtdVendas >= metaVendas:
+    print("A empresa vendeu {:.2f}mil e batemos a meta de vendas, parabéns".format(qtdVendas))
+else:
+    print("A empresa vendeu {:.2f} e por isso não bateu a meta esse mês, mais sorte na próxima".format(qtdVendas))
