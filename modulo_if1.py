a952c89beeb29d0f028887bcfdda271d97bfb1c1
Vinicius Mareti <110841400+ViniMaretti@users.noreply.github.com>
2024-02-21 11:08:13 -0300

    Python Impressionador

--- modulo_if1.py
+++ modulo_if1.py
@@ -2,6 +2,6 @@ metaVendas = 50.000
 qtdVendas = float(input('Digite a quantidade de vendas do iphone'))
 
 if qtdVendas >= metaVendas:
-    print("A empresa vendeu {:.2f}mil e batemos a meta de vendas, parabéns".format(qtdVendas))
+    print("A empresa vendeu {:.2f} e batemos a meta de vendas, parabéns".format(qtdVendas))
 else:
     print("A empresa vendeu {:.2f} e por isso não bateu a meta esse mês, mais sorte na próxima".format(qtdVendas))
